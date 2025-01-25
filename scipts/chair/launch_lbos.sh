c2bdb5ed5ddd0896c4e2d39155fa879c257d7d84
pwisneskey <paul.wisneskey@sagecons.com>
2025-01-24 21:23:33 -0700

    Added scripts for starting LBOS on Rasperry Pi with automatic mapping of control panel and hud touchscreens.

---
+++ scipts/chair/launch_lbos.sh
@@ -0,0 +1,10 @@
+
+export hudId=$(DISPLAY=:0 xinput | grep 'WaveShare WaveShare' | sed  -n "s/^.*id=\([[:digit:]]\+\).*$/\1/p")
+export cpId=$(DISPLAY=:0 xinput | grep 'WaveShare WS170120' | sed  -n "s/^.*id=\([[:digit:]]\+\).*$/\1/p")
+
+DISPLAY=:0 xinput -map-to-output $hudId HDMI-1
+DISPLAY=:0 xinput -map-to-output $cpId HDMI-2
+
+DISPLAY=:0 xhost +
+
+sudo DISPLAY=:0 java -jar lbos.jar -m CHAIR -p Chair
